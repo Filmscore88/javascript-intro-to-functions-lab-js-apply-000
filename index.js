c8b02374107471615127f2fcef25682b61279fde
Alberto Morales <cmtmorales@gmail.com>
2019-02-19 15:27:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function whisper(string){
   function sayHiToGrandma(string){
     if (string.toLowerCase()==string){
       return ("I can\'t hear you!")
-    if (string.ToUpperCase()==string){
+   } if (string.ToUpperCase()==string){
       return ("YES INDEED!")
     }
   }
